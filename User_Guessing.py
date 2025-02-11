272569ca0f53c8c3e3aebb58951bbd605a6b4a0e
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-11 17:51:20 +1000

    Feat: First iteration of User Guessing Game

---
+++ User_Guessing.py
@@ -0,0 +1,15 @@
+import random as rdm
+
+def Guess(x):
+    x = int(input('Maximum Number?: '))
+    random_number = rdm.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('Sorry, guess again. Too low.')
+        elif guess > random_number:
+            print('Sorry guess again. Too high.')
+    print(f'Congratulations, You have guessed the number {random_number} correctly!')
+
+Guess(x)
